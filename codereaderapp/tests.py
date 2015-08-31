3e288cf129636fb798610250c7a304b6fc5652bd
Rickard Lindberg <ricli85@gmail.com>
2015-08-31 09:53:00 +0200

    Move test code to appropriate module.

--- codereaderapp/tests.py
+++ codereaderapp/tests.py
@@ -1,3 +1,86 @@
-from django.test import TestCase
+from unittest.mock import sentinel
+import unittest
 
-# Create your tests here.
+from codereaderapp.annotations import Annotation
+from codereaderapp.annotations import Annotations
+from codereaderapp.annotations import Line
+
+
+class TestLinePartition(unittest.TestCase):
+
+    def test_no_annotations(self):
+        self.assertEqual(
+            Line(1, "hello").partition(Annotations([
+            ])),
+            [
+                {
+                    "text": "hello",
+                    "annotations": [],
+                },
+            ]
+        )
+
+    def test_one_annotation(self):
+        self.assertEqual(
+            Line(1, "hello").partition(Annotations([
+                Annotation(1, 1, 1, 2, sentinel.ANNOTATION),
+            ])),
+            [
+                {
+                    "text": "he",
+                    "annotations": [sentinel.ANNOTATION],
+                },
+                {
+                    "text": "llo",
+                    "annotations": [],
+                },
+            ]
+        )
+
+    def test_two_annotations(self):
+        self.assertEqual(
+            Line(1, "hello").partition(Annotations([
+                Annotation(1, 1, 1, 2, sentinel.ANNOTATION_1),
+                Annotation(1, 2, 1, 3, sentinel.ANNOTATION_2),
+            ])),
+            [
+                {
+                    "text": "h",
+                    "annotations": [sentinel.ANNOTATION_1],
+                },
+                {
+                    "text": "e",
+                    "annotations": [sentinel.ANNOTATION_1, sentinel.ANNOTATION_2],
+                },
+                {
+                    "text": "l",
+                    "annotations": [sentinel.ANNOTATION_2],
+                },
+                {
+                    "text": "lo",
+                    "annotations": [],
+                },
+            ]
+        )
+
+    def test_skips_annotations_where_line_does_not_match(self):
+        self.assertEqual(
+            Line(1, "hello").partition(Annotations([
+                Annotation(1, 1, 1, 2, sentinel.ANNOTATION_1),
+                Annotation(2, 1, 2, 3, sentinel.ANNOTATION_2),
+            ])),
+            [
+                {
+                    "text": "he",
+                    "annotations": [sentinel.ANNOTATION_1],
+                },
+                {
+                    "text": "llo",
+                    "annotations": [],
+                },
+            ]
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
