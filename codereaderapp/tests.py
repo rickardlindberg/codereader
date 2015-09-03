e6c10605aeed7c1bdf090c3af9ecef6cd4e18bde
Rickard Lindberg <ricli85@gmail.com>
2015-09-03 08:44:03 +0200

    Add tests.

--- codereaderapp/tests.py
+++ codereaderapp/tests.py
@@ -1,9 +1,22 @@
 import json
+import os.path
+import tempfile
 
 from django.test import TestCase
 
 
 class TestCodeReaderApp(TestCase):
+
+    def setUp(self):
+        from codereaderapp import views
+        self.tmp_dir = tempfile.TemporaryDirectory()
+        with open(os.path.join(self.tmp_dir.name, "README"), "w") as f:
+            f.write("line 1\n")
+            f.write("line 2\n")
+            f.write("line 3\n")
+            f.write("line 4\n")
+            f.write("line 5\n")
+        views.REPO_ROOT = self.tmp_dir.name
 
     def test_index(self):
         response = self.client.get("/")
@@ -11,12 +24,18 @@ class TestCodeReaderApp(TestCase):
 
     def test_file_list(self):
         json_response = self.get_json("/file_list")
-        self.assertIn("manage.py", json_response["files"])
+        self.assertEqual(json_response["files"], ["README"])
 
     def test_file(self):
-        json_response = self.get_json("/file/manage.py")
-        self.assertEqual(json_response["name"], "manage.py")
-        self.assertGreater(len(json_response["lines"]), 1)
+        json_response = self.get_json("/file/README")
+        self.assertEqual(json_response["name"], "README")
+        self.assertEqual(len(json_response["lines"]), 5)
+
+    def test_search(self):
+        json_response = self.get_json("/search/4")
+        self.assertEqual(json_response["term"], "4")
+        self.assertEqual(len(json_response["matches"]), 1)
+        self.assertEqual(json_response["matches"][0]["row"], 4)
 
     def get_json(self, url):
         return json.loads(self.client.get(url).content.decode("utf8"))
