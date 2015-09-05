5ff59b2c8e9ecbc8cd53f2fcadadc53f3020337d
Rickard Lindberg <ricli85@gmail.com>
2015-09-05 09:23:58 +0200

    Clean up.

--- codereaderlib/repo.py
+++ codereaderlib/repo.py
@@ -10,11 +10,11 @@ class Repo(object):
     def __init__(self, root):
         self._root = root
 
-    def search(self, term, limit=None):
+    def search(self, term, search_only_in=None):
         result = SearchResult(self._root, term)
         cmd = ["ack", "-H", "--column", "--output", "$&", term]
-        if limit is not None:
-            cmd.append(limit)
+        if search_only_in is not None:
+            cmd.append(search_only_in)
         ack_result = subprocess.check_output(cmd, cwd=self._root)
         for ack_line in ack_result.decode("utf-8").strip().split("\n"):
             parts = ack_line.split(":", 4)
