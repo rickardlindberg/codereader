a5f4f32a6e0b1d547552c7557fc178efc191444b
Rickard Lindberg <ricli85@gmail.com>
2015-09-05 09:22:12 +0200

    Clean up.

--- codereaderapp/views.py
+++ codereaderapp/views.py
@@ -22,16 +22,22 @@ def file_list(request):
 def file(request):
     name = request.GET.get("name")
     highlight = request.GET.get("highlight", "")
-    x = []
-    if len(highlight.strip()) > 0:
-        x = Repo(REPO_ROOT).search(highlight, name).get_annotations(name)
-    annotations = Annotations(
-        get_annotations(os.path.join(REPO_ROOT, name))
-        +
-        x
-    )
-    return JsonResponse(Repo(REPO_ROOT).get_file(name).render_file(annotations))
+    return JsonResponse(Repo(REPO_ROOT).get_file(name).render_file(_get_annotations(REPO_ROOT, name, highlight)))
 
 
 def search(request, term):
     return JsonResponse(Repo(REPO_ROOT).search(term).render())
+
+
+def _get_annotations(root, name, highlight):
+    return Annotations(
+        get_annotations(os.path.join(root, name))
+        +
+        _get_highligh_annotations(root, name, highlight)
+    )
+
+
+def _get_highligh_annotations(root, name, highlight):
+    if len(highlight.strip()) > 0:
+        return Repo(root).search(highlight, name).get_annotations(name)
+    return []
