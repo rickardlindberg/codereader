735771c56ff1a092b4b15f1b1ba39adfb3ec1628
Rickard Lindberg <ricli85@gmail.com>
2015-09-07 13:42:35 +0200

    REPO_ROOT is relative.

--- codereaderapp/views.py
+++ codereaderapp/views.py
@@ -8,7 +8,7 @@ from codereaderlib.annotations import Annotations
 from codereaderlib.repo import Repo
 
 
-REPO_ROOT = "."
+REPO_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 
 
 def index(request):
