c88fd70a1418e4ef271383353400171a89e98fb0
Rickard Lindberg <ricli85@gmail.com>
2015-09-08 09:00:05 +0200

    Support precompiling jsx file.

--- fabfile.py
+++ fabfile.py
@@ -1,11 +1,23 @@
 from fabric.api import *
 
 
+TRANSFORM_JSX_COMMAND = "jsx < codereaderapp/static/codereaderapp/index.jsx > codereaderapp/static/codereaderapp/index.js"
+
+
 def deploy(project_root):
-    local("python3 manage.py test")
+    test()
     local("git push")
     with cd(project_root):
         run("git pull")
+        run(TRANSFORM_JSX_COMMAND)
         run("python3 manage.py collectstatic --clear --noinput")
         run("python3 manage.py migrate")
         run("python3 manage.py test")
+
+def test():
+    transformjsx()
+    local("python3 manage.py test")
+
+
+def transformjsx():
+    local(TRANSFORM_JSX_COMMAND)
